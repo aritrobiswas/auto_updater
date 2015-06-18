ce38beb5f1d5bddfb756a5510fe91138814b4eac
Aritro Biswas <aritrobiswas@Aritros-MacBook-Pro.local>
2015-06-18 18:46:16 -0400

    with updated backuputil function

--- build/lib/backup/auto_updater.py
+++ build/lib/backup/auto_updater.py
@@ -2,32 +2,10 @@ import os
 import sys
 from optparse import OptionParser
 
-# The top argument for walk. The
-# Python27/Lib/site-packages folder in my case
-
-topdir = '.' #'./temp/backup_practice' # actual directory on my computer: '/home/aritrobiswas/projects/Markdowns/temp/backup_practice'
-
-# The arg argument for walk, and subsequently ext for step
-exten = '.txt'
-
-
-def step(ext, dirname, names):
-    ext = ext.lower()
-
-    for name in names:
-        if name.lower().endswith(ext):
-            file_path = os.path.join(dirname, name)
-            os.system("gsutil -m cp " + file_path + " gs://neimanmarcus/aritro/")
-
-# Start the walk
-def backup_files_with_walk():
-        inf = open("cloud_location.txt")
-        location = inf.read()
-        os.path.walk(topdir, step, exten)
-
-
 def backup_files(backup_dir,bucket):
-        os.system("gsutil -m cp -R " + backup_dir + " " + cloud_location)
+    """ Backs up all files in the specified directory to the specified bucket
+    """
+    os.system("gsutil -m cp -R " + backup_dir + " " + bucket)
 
 
 def parse_options(argv):
@@ -41,4 +19,4 @@ def parse_options(argv):
 
 if __name__ == "__main__":
         backup_dir,bucket = parse_options(sys.argv)
-        backup_files(backup_dir,bucket)                                     
+        backup_files(backup_dir,bucket)
--- build/lib/backup/set_cron_job.py
+++ build/lib/backup/set_cron_job.py
@@ -3,6 +3,10 @@ import sys
 import inspect
 
 def set_cron_job(backup_time,backup_dir,cloud_loc):
+        """
+        Sets a cron job at the specified backup_time to back up the
+        files in the specified directory at the specified cloud location
+        """
         os.system("sudo touch cron_container.txt")
         os.system("crontab -l > cron_container.txt")
         crontab_cmd = backup_time + " sudo backuputil -p " + backup_dir + " -b " + cloud_loc
@@ -12,7 +16,7 @@ def set_cron_job(backup_time,backup_dir,cloud_loc):
 
 
 if __name__ == "__main__":
-        print os.getcwd()
+        #print os.getcwd()
         backup_dir = input("Enter directory to be backed up: ")
         backup_time = input("Enter backup time with the time fields in the appropriate locations in cron format'Minute Hour (Day of Month) Month (Day of Week)' with exactly 1 space between each of the time fields:  ")
         cloud_loc = input("Enter cloud location to back up to: ")
